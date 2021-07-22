1ba1af42541c3e4a24c3f987042e74ed68e88215
Amanda Ferreira <amandasilvaf1995@gmail.com>
2021-07-22 15:14:30 -0300

    Introduz o uso de css em templates blade

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -5,58 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>View</title>
-    <style>
-          .row {
-              display: flex;
-            }
-          
-          .col1 {
-            flex: 30%;
-            padding: 10px;
-          }
-          
-          .col2 {
-            flex: 70%;
-            padding: 10px;
-          }
-          
-          .menu ul
-          {
-            margin: 0px;
-            padding: 0px;
-            list-style-type: none;
-          }
-          
-          .menu li
-          {
-            list-style: none;
-          }
-          
-          .menu a
-          {
-            display: block;
-            width: 200px;
-            height: 20px;
-            color: black;
-            background-color: #FFFFFF;
-            text-decoration: none;
-            text-align: center;
-            margin: 5px;
-            font-size: 18px;
-          }
-          
-          .menu a:hover
-          {
-            background-color: #b9bcd0;
-            color: #0000ff;
-            font-weight: bold;
-          }
-          
-          .menu .active
-          {
-            background-color: #c0d4f3;
-          }
-    </style>
+    <link rel="stylesheet" href="{{asset('css/principal.css')}}">
 </head>
 <body>
   <div class="row">
