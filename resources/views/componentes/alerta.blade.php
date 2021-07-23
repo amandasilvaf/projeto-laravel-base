5544dd61cdded7f0fca7a88b870e57ce2a1e45fa
Amanda Ferreira <amandasilvaf1995@gmail.com>
2021-07-23 11:12:43 -0300

    Passa parametros para o componente
    por meio de um array associativo no @component (no template principal), contendo o nome das variaveis e seus respectivos valores. E recebendo esses valores no componente, na tag html com {{}}

--- resources/views/componentes/alerta.blade.php
+++ resources/views/componentes/alerta.blade.php
@@ -1,4 +1,5 @@
-<div class="box info">
-    <div class="title">Titulo do alerta</div>
-    <div class="msg">Texto do alerta</div>
+<div class="box {{$tipo}}">
+    <div class="title"> {{$titulo}} </div>
+    <div class="msg"> {{$slot}} </div>
 </div>
+
--- resources/views/outras/departamentos.blade.php
+++ resources/views/outras/departamentos.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.principal')
+
 @section('conteudo')
 @section('titulo', 'Departamentos')
     <h3>Departamentos:</h3>
@@ -9,9 +10,20 @@
         <li>Acessórios</li>
     </ul>
 
-@component('componentes.alerta')
-    
+
+@component('componentes.alerta', ['titulo'=> 'ERROR', 'tipo'=>'error'])
+<p><strong>Erro inesperado</strong></p>
+<p>Ocorreu um erro inesperado</p>
 @endcomponent
 
+@component('componentes.alerta', ['titulo'=> 'ALERTA', 'tipo'=>'warning'])
+<p><strong>Alerta para vírus</strong></p>
+<p>Seu pc está sem antivírus</p>
+@endcomponent
+
+@component('componentes.alerta', ['titulo'=> 'SUCESSO', 'tipo'=>'success'])
+<p><strong>Sucesso ao salvar</strong></p>
+<p>Arquivo salvo com sucesso</p>
+@endcomponent
 
 @endsection
