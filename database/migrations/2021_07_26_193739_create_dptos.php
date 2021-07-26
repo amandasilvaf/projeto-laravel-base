e169fc51420ad0e80c07eb135eb9a99893c1289f
Amanda Ferreira <amandasilvaf1995@gmail.com>
2021-07-26 17:55:27 -0300

    Cria Relacionamento N para N

---
+++ database/migrations/2021_07_26_193739_create_dptos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDptos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departamentos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('descricao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departamentos');
+    }
+}
---
+++ database/migrations/2021_07_26_194000_create_produto_departamento.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDepartamento extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_departamento', function (Blueprint $table) {
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('departamento_id');
+            $table->foreign('product_id')->references('id')
+                ->on('products');
+            $table->foreign('departamento_id')->references('id')
+                ->on('departamentos');
+            $table->primary(['product_id', 'departamento_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_departamento');
+    }
+}
