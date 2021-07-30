afb195d8b49b18d2b0fca64b5acf9c7f2e72f3a2
Amanda Ferreira <amandasilvaf1995@gmail.com>
2021-07-30 17:10:20 -0300

    Relaciona 1:1
    Primeira parte

---
+++ database/migrations/2021_07_30_193435_create_enderecos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->integer('cliente_id')->unsigned();
+            $table->foreing('cliente_id')->references('id')->on('clientes');
+            $table->primary('cliente_id');
+            $table->string('rua');
+            $table->integer('numero');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('uf');
+            $table->string('cep');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
