5e93f717ed5f7f633ee0d5bc686b609824e35109
Amanda Ferreira <amandasilvaf1995@gmail.com>
2021-08-03 17:39:20 -0300

    Altera o nome default de uma tabela
    Por padrao, as tabelas sao nomeadas pelo laravel com base no nome dado ao Model. O nome de uma tabela e sempre o nome do Model correspondente acrescentado (s), simulando assim, o plural. Nesse commit, altero o nome default das tabelas desenvolvedores e alocacoes, atribuindo os nomes no atributo table da Model.

--- app/Models/Alocacao.php
+++ app/Models/Alocacao.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alocacao extends Model
 {
+    protected $table = 'alocacoes';
     use HasFactory;
 }
--- app/Models/Desenvolvedor.php
+++ app/Models/Desenvolvedor.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Desenvolvedor extends Model
 {
+    protected $table = 'desenvolvedores';
     use HasFactory;
 }
